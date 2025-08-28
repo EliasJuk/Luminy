3685f8b1ee941cbf638049b5152576bd0c7e0271
eliasjuk <eliasp.juk@hotmail.com>
2025-08-28 19:35:31 -0300

    struct for blocks

---
+++ core/src/modules/block.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+use super::transaction::Transaction;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+  pub index: u64,
+  pub timestamp: u128,
+  pub transactions: Vec<Transaction>,
+  pub previous_hash: String,
+  pub nonce: u64,
+  pub hash: String,
+}
